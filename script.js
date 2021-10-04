94d720aaa54d63ebf5933fcfb1eee7a18bc7f5a3
janis21111 <70441380+janis21111@users.noreply.github.com>
2021-10-04 09:12:14 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,20 +8,25 @@ window.addEventListener('DOMContentLoaded', function(){
                 var engine = new BABYLON.Engine(canvas, true);
                 var createScene = function(){
                     var scene = new BABYLON.Scene(engine);
-                    
                     scene.clearColor = new BABYLON.Color3.White();
-                    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.
-                    Vector3(0, 4,-10), scene);
+
+                    var box = BABYLON.Mesh.CreateBox("Box",4.0,scene);
+                    // var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 4,-10), scene);
+                    var camera = new BABYLON.ArcRotateCamera("arcCamera", 
+                        BABYLON.Tools.ToRadians(45),
+                        BABYLON.Tools.ToRadians(45),
+                        10.0, box.position, scene )
+
                     camera.setTarget(BABYLON.Vector3.Zero());
                     camera.attachControl(canvas, true);
 
-                    // controlls setup for camera
+                    // controlls setup for
                     camera.keysUp.push(87);// w - for up
                     camera.keysDown.push(83);// S - for down
                     camera.keysLeft.push(65);// A - for left
                     camera.keysRight.push(68);// D - for right
 
-                    var box = BABYLON.Mesh.CreateBox("Box",4.0,scene);
+
                     
                     return scene;
                 }
