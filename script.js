efb480544543e61d6c84ddeb7a17e1c90326305b
janis21111 <70441380+janis21111@users.noreply.github.com>
2021-10-04 08:51:38 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+console.log("Helllooooo");
+
+
+window.addEventListener('DOMContentLoaded', function(){
+                var canvas = document.getElementById('canvas');
+      
+                var engine = new BABYLON.Engine(canvas, true);
+                var createScene = function(){
+                    var scene = new BABYLON.Scene(engine);
+                    
+                    scene.clearColor = new BABYLON.Color3.White();
+                    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.
+                    Vector3(0, 4,-10), scene);
+                    camera.setTarget(BABYLON.Vector3.Zero());
+                    camera.attachControl(canvas, true);
+
+                    // controlls setup for camera
+                    camera.keysUp.push(87);// w - for up
+                    camera.keysDown.push(83);// S - for down
+                    camera.keysLeft.push(65);// A - for left
+                    camera.keysRight.push(68);// D - for right
+
+                    var box = BABYLON.Mesh.CreateBox("Box",4.0,scene);
+                    
+                    return scene;
+                }
+      
+                var scene = createScene();
+                engine.runRenderLoop(function(){
+                    scene.render();
+                });
+      
+            });
